d688266045304695c6d03819d6bbf3c227c84cf7
moutusi <moutusi.slg01@gmail.com>
2024-09-14 17:08:11 +0530

    converted csv file

---
+++ src/data/conversion.js
@@ -0,0 +1,28 @@
+
+const fs = require('fs');
+const path = require('path');
+const csv = require('csv-parser');
+
+
+const matchesPath = path.join(__dirname, 'matches.csv');
+const deliveriesPath = path.join(__dirname, 'deliveries.csv');
+
+
+function csvToObjectSync(filePath) {
+  const results = [];
+  fs.createReadStream(filePath)
+    .pipe(csv())
+    .on('data', (data) => results.push(data))
+    .on('end', () => {
+     const writeStream = fs.createWriteStream(filePath.replace('.csv','.js'));
+     writeStream.write(JSON.stringify(results, null, 2), 'utf-8')
+    });
+}
+
+
+function convertAndWriteFilesSync() {
+  csvToObjectSync(matchesPath);
+  csvToObjectSync(deliveriesPath);
+}
+
+convertAndWriteFilesSync();
