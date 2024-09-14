db30d9c505bd5c08124d7ecf9d21f6bc01bf2c3d
moutusi <moutusi.slg01@gmail.com>
2024-09-14 17:13:37 +0530

    question 1 done

---
+++ src/server/matches-per-year.js
@@ -0,0 +1,28 @@
+const fs = require('fs');
+const path = require('path');
+const matches = require('../data/matches.js');
+// console.log(matches);
+
+const outputFilePath = path.join(__dirname, '../../src/public/output/matchesPerYear.json');
+
+function testMatchesPerYear() {
+    const matchesPerYear = {};
+    for (let i = 0; i < matches.length; i++) {
+        const season = matches[i].season;
+        if (!matchesPerYear[season]) {
+            matchesPerYear[season] = 0;
+        }
+        matchesPerYear[season]++;
+    }             
+                   
+    return matchesPerYear;
+}
+const data = testMatchesPerYear();
+fs.writeFileSync(outputFilePath, JSON.stringify(data, null, 2), 'utf8');
+
+console.log('success', outputFilePath);
+
+
+
+
+
