ad2e7587977c9094fb6a207d40e2d09e6617e30a
moutusi <moutusi.slg01@gmail.com>
2024-09-23 18:27:34 +0530

    added try catch handler

--- src/server/output.js
+++ src/server/output.js
@@ -3,10 +3,17 @@ const path = require('path');
 
 
 function json(filepath, data) {
-    const outputFilePath = path.join(__dirname, filepath);
-    fs.writeFileSync(outputFilePath, JSON.stringify(data, null, 2), 'utf8');
-
-    console.log('success', outputFilePath);
+    try{
+        const outputFilePath = path.join(__dirname, filepath);
+        fs.writeFileSync(outputFilePath, JSON.stringify(data, null, 2), 'utf8');
+    
+        console.log('success', outputFilePath);
+    }
+    catch(err){
+        console.error(err);
+        
+    }
+   
 }
 
 module.exports = json
